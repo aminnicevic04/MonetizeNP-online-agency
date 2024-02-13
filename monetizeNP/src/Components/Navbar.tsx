bb02584ad3c193d3bb6cd858434bb3b32668ef4b
aminnicevic04 <aminnicevic@gmail.com>
2024-02-13 21:10:29 +0100

    update

--- monetizeNP/src/Components/Navbar.tsx
+++ monetizeNP/src/Components/Navbar.tsx
@@ -1,10 +1,16 @@
 import React, { useEffect } from "react";
 import { useState } from "react";
-import logo from "../assets/logo.png";
+import logo from "../assets/logo2.png";
+import { Link } from "react-scroll";
+import "./animation.css";
+
+import { FaXmark, FaBars } from "react-icons/fa6";
 
 function Navbar() {
   const toggleMenu = () => {
-    setIsMenuOpen(true);
+    if (isMenuOpen === false) {
+      setIsMenuOpen(true);
+    } else setIsMenuOpen(false);
   };
 
   const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
@@ -35,9 +41,69 @@ function Navbar() {
 
   return (
     <header className="w-full bg-white md:bg-transparent fixed top-0 left-0 right-0">
-      <nav className="w-full bg-green-500">
-        <div className="bg-red-500 w-full ">
-          <img src={logo} alt="Logo" className="w-20 bg-cyan-500 "></img>
+      <nav
+        className={`py-0 lg:px-14 px-4 ${
+          isSticky
+            ? "sticky top-0 left-0 right-0 border-b bg-white duration-300"
+            : ""
+        }`}
+      >
+        <div className="flex justify-between items-center text-base gap-8">
+          <img
+            src={logo}
+            alt="Logo"
+            className="w-80 left-0 transform-gpu translate-y-0"
+          ></img>
+          <ul className="md:flex space-x-12 hidden">
+            {navItems.map(({ link, path }) => (
+              <Link
+                to={path}
+                key={path}
+                spy={true}
+                smooth={true}
+                offset={-100}
+                className="uppercase block text-lg text-gray900 hover:text-brandPrimary font-medium cursor-pointer link-animation"
+              >
+                {link}
+              </Link>
+            ))}
+          </ul>
+
+          <div className="space-x-12 hidden lg:flex items-center">
+            <button className="bg-brandPrimary text-white py-2 px-4 transition-all duration-300 rounded hover:bg-neutralGrey">
+              Contact US
+            </button>
+          </div>
+          <div className="md:hidden">
+            <button
+              onClick={toggleMenu}
+              className="text-neutralDGrey focus:outline-none focus:text-gray-500 m-8"
+            >
+              {isMenuOpen ? (
+                <FaXmark className="h-6 w-6 mr-6" />
+              ) : (
+                <FaBars className="h-6 w-6 mr-6" />
+              )}
+            </button>
+          </div>
+        </div>
+        <div
+          className={`space-y-4 px-7 mt-40 py-10 bg-brandPrimary ${
+            isMenuOpen ? "block fixed top-0 right-0 left-0" : "hidden"
+          }`}
+        >
+          {navItems.map(({ link, path }) => (
+            <Link
+              to={path}
+              key={path}
+              spy={true}
+              smooth={true}
+              offset={-100}
+              className="block text-xl text-white hover:text-brandPrimary uppercase cursor-pointer"
+            >
+              {link}
+            </Link>
+          ))}
         </div>
       </nav>
     </header>
