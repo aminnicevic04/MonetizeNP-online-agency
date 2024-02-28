91b81765b3505974f437f99cf23c198a8870ebd1
aminnicevic04 <aminnicevic@gmail.com>
2024-02-28 16:44:37 +0100

    upadate

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -43,7 +43,7 @@ function Navbar() {
   ];
 
   return (
-    <header className="w-full bg-white md:bg-transparent fixed top-8 left-0 right-0">
+    <header className="w-full bg-white md:bg-transparent fixed top-0 left-0 right-0">
       <nav
         className={`py-0 lg:px-14 ${
           isSticky ? "sticky top-0 left-0 right-0 bg-dark1 duration-300" : ""
