111ccb2102f6b73320581bcc6317ddd5cdfc83d6
aminnicevic04 <aminnicevic@gmail.com>
2024-03-03 20:34:58 +0100

    update

--- src/Components/MyFooter.tsx
+++ src/Components/MyFooter.tsx
@@ -1,6 +1,6 @@
-import React from "react";
+// import React from "react";
 import { Footer } from "flowbite-react";
-import LOGO from "../assets/logo2.png";
+// import LOGO from "../assets/logo2.png";
 
 function MyFooter() {
   return (
